a849b9ff7195e685749f6caef37f102ba23a8eda
felixseg <felix.seg@gmx.de>
2017-01-13 10:53:51 +0100

    Added JavaScript

--- func.js
+++ func.js
@@ -1,1 +1,9 @@
-//ich bin da
+function seturl () {
+  var senseboxid = document.getElementById("senseID").value;
+ 
+  apiurl = "https://api.opensensemap.org/boxes/" + senseboxid;
+ 
+  alert(apiurl);
+}
+
+document.getElementById("sendSenseID").onclick = seturl;
